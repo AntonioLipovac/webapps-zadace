a888d323c0134015aa873f90b1c0d8b57ba18695
AntonioLipovac <75137131+AntonioLipovac@users.noreply.github.com>
2022-10-12 00:39:27 +0200

    Riješen zadatak 8

---
+++ 01-zadaci/zadatak8.js
@@ -0,0 +1,13 @@
+//Napiši funkciju koja vraća predano polje obrnutim redosljedom
+//Ispis : [3,4,5,6] -> [6,5,4,3]
+normalno_polje = [3,4,5,6];
+obrnuto_polje = [];
+function ReversePolje(normalno_polje) {
+  for (let i of normalno_polje) {
+    obrnuto_polje.unshift(i);
+  }
+  for (let j of obrnuto_polje) {
+    console.log(j);
+  }
+}
+ReversePolje(normalno_polje);
